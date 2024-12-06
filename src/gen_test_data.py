015216cedd6cc1779e550d02c8dcd47406f8a80c
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2024-12-06 01:30:56 -0500

    removing numpy calculations for accuracy

--- src/gen_test_data.py
+++ src/gen_test_data.py
@@ -9,29 +9,8 @@ np.random.seed(0xCA7CAFE)
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 tilesize = 32
-sizes = []
-test_sizes=[(tilesize,tilesize),(tilesize,2*tilesize),(2*tilesize,tilesize),(tilesize*2,tilesize*2)]
-
-
-
-with open(os.path.join(script_dir, "sizes.json"), "w") as f:
-    json.dump(
-        [
-            {"size": size_i, "size_j":size_i}
-            for size_i in sizes
-        ],
-        f,
-        indent=2,
-    )
-with open(os.path.join(script_dir, "test_sizes.json"), "w") as f:
-    json.dump(
-        [
-            {"size_i": size_i, "size_j":size_j}
-            for (size_i,size_j) in test_sizes
-        ],
-        f,
-        indent=2,
-    )
+sizes = [512,1024,2048,4096,8192]
+test_sizes=[(tilesize,tilesize),(tilesize,2*tilesize),(2*tilesize,tilesize),(tilesize*2,tilesize*2), (128,128)]
 
 
 
@@ -40,33 +19,17 @@ for (size_i,size_j) in test_sizes:
     a_fname = os.path.join(script_dir, f"test_a_{size_i}_{size_j}.bin")
     with open(a_fname, "wb") as f:
         f.write(a.tobytes())
-    c = np.transpose(np.linalg.qr(a, mode = "raw")[0])
-    c_fname = os.path.join(script_dir, f"test_ref_{size_i}_{size_j}.bin")
-    with open(c_fname, "wb") as f:
-        f.write(c.tobytes())
-    x = np.random.randn(size_i, tilesize).astype(np.float32)
+    x = np.random.randn(size_i, size_j).astype(np.float32)
     x_fname = os.path.join(script_dir, f"test_x_{size_i}_{size_j}.bin")
     with open(x_fname, "wb") as f:
         f.write(x.tobytes())
-    d = np.transpose(np.linalg.qr(a, mode="complete")[0]).dot( x)
-    d_fname = os.path.join(script_dir, f"test_xmul_{size_i}_{size_j}.bin")
-    with open(d_fname, "wb") as f:
-        f.write(d.tobytes())
 
 for size_i in sizes:
     a = np.random.randn(size_i, size_i).astype(np.float32)
     a_fname = os.path.join(script_dir, f"test_a_{size_i}_{size_i}.bin")
     with open(a_fname, "wb") as f:
         f.write(a.tobytes())
-    c = np.transpose(np.linalg.qr(a, mode = "raw")[0])
-    c_fname = os.path.join(script_dir, f"test_ref_{size_i}_{size_i}.bin")
-    with open(c_fname, "wb") as f:
-        f.write(c.tobytes())
-    x = np.random.randn(size_i,tilesize).astype(np.float32)
+    x = np.random.randn(size_i,size_i).astype(np.float32)
     x_fname = os.path.join(script_dir, f"test_x_{size_i}_{size_i}.bin")
     with open(x_fname, "wb") as f:
         f.write(x.tobytes())
-    d = np.transpose(np.linalg.qr(a, mode="complete")[0]).dot( x)
-    d_fname = os.path.join(script_dir, f"test_xmul_{size_i}_{size_i}.bin")
-    with open(d_fname, "wb") as f:
-        f.write(d.tobytes())
